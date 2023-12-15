37bfd091650c027159e869e76fdd1dd736112029
Mukesh Panchal <mukeshpanchal27@users.noreply.github.com>
2023-12-15 11:12:04 +0530

    Update plugin-check.php

--- plugin-check.php
+++ plugin-check.php
@@ -81,3 +81,44 @@ function wp_plugin_check_display_composer_autoload_notice() {
 }
 
 wp_plugin_check_load();
+
+/**
+ * Callback function that print plugin activation script.
+ *
+ * @since n.e.x.t
+ */
+function perflab_print_plugin_activation_script() {
+	$js = <<<JS
+( function( $ ) {
+	$( document ).ajaxComplete( function( event, xhr, settings ) {
+		// Check if this is the 'install-plugin' request.
+		if ( settings.data && typeof settings.data === 'string' && settings.data.includes( 'action=install-plugin' ) ) {
+			var target_element = $( event.target.activeElement );
+			if ( ! target_element ) {
+				return;
+			}
+			/*
+			 * WordPress core uses a 1s timeout for updating the activation link,
+			 * so we set a 1.5 timeout here to ensure our changes get updated after
+			 * the core changes have taken place.
+			 */
+			setTimeout( function() {
+				var plugin_url = target_element.attr( 'href' );
+				if ( ! plugin_url ) {
+					return;
+				}
+				var nonce = target_element.attr( 'data-plugin-activation-nonce' );
+				var plugin_slug = target_element.attr( 'data-slug' );
+				var url = new URL( plugin_url );
+				url.searchParams.set( 'action', 'perflab_activate_plugin' );
+				url.searchParams.set( '_wpnonce', nonce );
+				url.searchParams.set( 'plugin', plugin_slug );
+				target_element.attr( 'href', url.href );
+			}, 1500 );
+		}
+	} );
+} )( jQuery );
+JS;
+
+	wp_print_inline_script_tag( $js );
+}
